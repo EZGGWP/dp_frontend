73666582e7dcdb2932ddacd4d921a6ca88392292
EZGGWP <nikita.be2011@yandex.ru>
2024-05-25 20:13:31 +0300

    Remove unused import

--- src/pages/mainPage/components/navbar.tsx
+++ src/pages/mainPage/components/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import { Button, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { LoginButton } from './loginButton';
 
 export default () => {
