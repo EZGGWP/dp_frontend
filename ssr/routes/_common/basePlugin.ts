94e1a1df926c98fb580cb6af19765e4af1fb7b96
EZGGWP <nikita.be2011@yandex.ru>
2024-05-25 17:29:02 +0300

    Fix codestyle

--- ssr/routes/_common/basePlugin.ts
+++ ssr/routes/_common/basePlugin.ts
@@ -7,11 +7,11 @@ export abstract class BasePlugin {
 
     public abstract routeHandler(request: FastifyRequest, response: FastifyReply): void;
     public plugin(fastify: FastifyInstance, opts: FastifyPluginOptions, done: (err?: any) => void) {
-        fastify.get(this.path, this.routeHandler);
-        done();
+      fastify.get(this.path, this.routeHandler);
+      done();
     }
 
     public getPluginRegistrationData(opts?: FastifyPluginOptions) {
-        return { plugin: this.plugin.bind(this), opts: { ...this.opts, ...opts }, routeMeta: { path: this.path } };
+      return { plugin: this.plugin.bind(this), opts: { ...this.opts, ...opts }, routeMeta: { path: this.path } };
     }
 }
--- ssr/routes/v1/index.ts
+++ ssr/routes/v1/index.ts
@@ -1,11 +1,11 @@
 import { TestPlugin } from './test/plugin';
 
 const defaultOptions = {
-    prefix: 'v1',
+  prefix: 'v1',
 }
 
 const routes = [
-    (new TestPlugin).getPluginRegistrationData(defaultOptions),
+  (new TestPlugin).getPluginRegistrationData(defaultOptions),
 ]
 
 export default routes;
